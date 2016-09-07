0943d7b39dcbb5514e9ada602b1079ddc20eb873
cgroll <groll.christian.edu@gmail.com>
2016-09-07 09:02:37 +0200

    render ticker symbols useless

--- daxDownload.m
+++ daxDownload.m
@@ -5,8 +5,8 @@ dateBeg = '01011990';
 dateEnd = '01092016';
 
 % specify assets
-tickSymb1 = '^GDAXI';
-tickSymb2 = '^GSPC';
+tickSymb1 = '^JJJJ';
+tickSymb2 = '^KKKK';
 tickSymbs = {tickSymb1; tickSymb2};
 
 data = getPrices(dateBeg, dateEnd, tickSymbs);
